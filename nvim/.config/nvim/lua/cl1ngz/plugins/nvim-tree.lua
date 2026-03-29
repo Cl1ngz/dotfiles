041a08777fcaae51d1707b239c561e5310d6e445
Cl1ngz <ByteIgma@gmail.com>
2026-03-30 00:08:06 +0200

    nvim tree telescope fix and some file managment

--- nvim/.config/nvim/lua/cl1ngz/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/cl1ngz/plugins/nvim-tree.lua
@@ -3,14 +3,35 @@ return {
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
     require("nvim-tree").setup({
-      view = { width = 30 },
+      -- 1. Disable git ignore filtering
+      git = {
+        enable = true,
+        ignore = false, -- This ensures .gitignore files are SHOWN
+        timeout = 400,
+      },
+      -- 2. Disable dotfile and custom filtering
+      filters = {
+        dotfiles = false, -- This ensures .hidden files are SHOWN
+        custom = {}, -- Ensures no specific patterns are hidden
+      },
+      view = {
+        width = 30,
+      },
       renderer = {
-        indent_markers = { enable = true },
+        indent_markers = {
+          enable = true,
+        },
       },
       actions = {
-        open_file = { window_picker = { enable = false } },
+        open_file = {
+          window_picker = {
+            enable = false,
+          },
+        },
       },
     })
+
+    -- Keep your existing keymap
     vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle explorer" })
   end,
 }
--- nvim/.config/nvim/lua/cl1ngz/plugins/telescope.lua
+++ nvim/.config/nvim/lua/cl1ngz/plugins/telescope.lua
@@ -1,42 +1,33 @@
 return {
   "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
+  -- Removed branch = "0.1.x" to get the latest 2026 fixes for Treesitter v1.0
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
     "folke/todo-comments.nvim",
-    { "folke/trouble.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } }, -- Add trouble.nvim here
+    { "folke/trouble.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
   },
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
-    local transform_mod = require("telescope.actions.mt").transform_mod
-
-    -- Error handling for trouble
-    local ok, trouble = pcall(require, "trouble")
-    if not ok then
-      print("Trouble not loaded")
-      return
-    end
-    local trouble_telescope = require("trouble.sources.telescope")
-
-    -- Custom action
-    local custom_actions = transform_mod({
-      open_trouble_qflist = function(prompt_bufnr)
-        trouble.toggle("quickfix")
-      end,
-    })
 
     telescope.setup({
       defaults = {
         path_display = { "smart" },
+        -- This part ensures the previewer handles the new Treesitter correctly
+        preview = {
+          treesitter = true,
+          filesize_limit = 0.1, -- MB
+        },
         mappings = {
           i = {
             ["<C-k>"] = actions.move_selection_previous,
             ["<C-j>"] = actions.move_selection_next,
-            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-            ["<C-t>"] = trouble_telescope.open,
+            ["<C-q>"] = actions.send_selected_to_qflist,
+            ["<C-t>"] = function(prompt_bufnr)
+              require("trouble.sources.telescope").open(prompt_bufnr)
+            end,
           },
         },
       },
@@ -45,14 +36,12 @@ return {
     telescope.load_extension("fzf")
     telescope.load_extension("todo-comments")
 
-    -- Set keymaps
+    -- Keymaps
     local keymap = vim.keymap
-
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
   end,
 }
-
